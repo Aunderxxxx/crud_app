54908575b984dcb1ea67aef98bb9a641d296cb8b
Aung Myint Myat <ammyat-it@outlook.com>
2023-06-08 21:42:06 +0630

    dev update

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+Route::get('', function(){
+    return redirect('client');
+});
 
 //client crud
 Route::resource('client', \App\Http\Controllers\ClientController::class);
